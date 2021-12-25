a21fde47c97a9e2fb084176121526734af1fdfb8
Christie Tsang <ctsang195@gmail.com>
2021-12-24 22:30:14 -0800

    Add design elements to Portfolio Page

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,14 @@
 import "./intro.scss"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            "intro"
+                <div className="itemContainer">
+                    <a href="#portfolio">
+                        <KeyboardArrowDownIcon className="icon"/>
+                    </a>
+                </div>
         </div>
     )
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,22 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className = "portfolio" id="portfolio">
-            "Portfolio"
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Web App</li>
+                <li>Games</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://dummyimage.com/200x200/000000/fff" alt=""/>
+                    <h3>PandemicTravel</h3>
+                </div>
+                <div className="item">
+                    <img src="https://dummyimage.com/200x200/000000/fff" alt=""/>
+                    <h3>DeskBuddy</h3>
+                </div>
+            </div>
         </div>
     )
 }
