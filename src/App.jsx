340383fe50d41e2e2f3be83951de7264c2289d37
Christie Tsang <ctsang195@gmail.com>
2021-12-24 19:02:16 -0800

    implement side navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Topbar from "./components/topbar/topbar";
+import Menu from "./components/intro/menu/Menu";
 import Contact from "./components/contact/contact";
 import Intro from "./components/intro/intro";
 import Portfolio from "./components/portfolio/portfolio";
 import Testimonials from "./components/testimonials/testimonials";
 import Works from "./components/works/works";
 import "./app.scss"
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(true)
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="section">
         <Intro/>
         <Portfolio/>
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -3,9 +3,9 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="title">my<span class="title-color">Portfolio.</span></a>
@@ -20,7 +20,7 @@ export default function Topbar() {
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
