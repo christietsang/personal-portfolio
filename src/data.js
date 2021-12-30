16a304e7f345857e870be17a7bf07bfd8d73def2
Christie Tsang <ctsang195@gmail.com>
2021-12-29 16:08:21 -0800

    Add media queries for portfolio

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const webAppPortfolio = [{
     id: 1,
     title: "PandemicTravel",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    link: "https://facebook.ca",
+    img: "https://i.ibb.co/YtyV2yF/Pandemic-Travel.png",
+    link: "https://wt2021-6eddd.web.app/",
 },
 {
     id: 2,
     title: "DeskBuddy",
-    img: "https://dummyimage.com/200x200/000000/fff",
-    link: "https://linkedin.ca",
+    img: "https://i.ibb.co/pR5W8mh/Desk-Buddy.png",
+    link: "https://desk-buddy-99890.web.app/",
 },
 ];
 
 export const gameAppPortfolio = [{
     id: 1,
     title: "BCIT Simulator",
-    img: "https://dummyimage.com/200x200/000000/fff",
+    img: "https://i.ibb.co/w4pTWTd/BCITSimulator.png",
     link: "https://twitter.ca",
 },
 ];
@@ -23,8 +23,8 @@ export const gameAppPortfolio = [{
 export const designPortfolio = [{
     id: 1,
     title: "PandemicTravel Design",
-    img: "https://dummyimage.com/200x200/000000/fff",
-    link: "https://google.ca.ca",
+    img: "https://i.ibb.co/sjGRFft/Pandemic-Travel-Figma.png",
+    link: "https://www.figma.com/file/4EK0OIAYEbB1tf9sf7A61C/PandemicTravel?node-id=85%3A134",
 },
 {
     id: 2,
@@ -35,7 +35,7 @@ export const designPortfolio = [{
 {
     id: 3,
     title: "DeskBuddy Design",
-    img: "https://dummyimage.com/200x200/000000/fff",
-    link: "https://linkedin.ca",
+    img: "https://i.ibb.co/wMjM7xQ/Desk-Buddy-Figma.png",
+    link: "https://www.figma.com/file/efhZ9rUo5x8S3xrBn3jEA7/Demo---Figma?node-id=2%3A3",
 },
 ];
