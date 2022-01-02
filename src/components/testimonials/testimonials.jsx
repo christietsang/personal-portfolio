02fe18d1764d30b62ed0beb51b0deeed1587fbd1
Christie Tsang <ctsang195@gmail.com>
2022-01-01 19:26:26 -0800

    Add content to testimonial

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,32 +1,113 @@
-
 import React from "react";
 import "./testimonials.scss"
 import OwlCarousel from 'react-owl-carousel'
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import ScreenHeading from '../../utilities/headings/ScreenHeading'
+import FormatQuoteRoundedIcon from '@mui/icons-material/FormatQuoteRounded';
 
 export default function Testimonials() {
+
+    const carousel_options = {
+        loop: true,
+        margin: 0,
+        dots: true,
+        autoplay: true,
+        animateIn: "bounceInRight",
+        animateOut: "bounceOutRight",
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+            },
+        }
+    }
     return (
-        <div className = "testimonial" id="testimonials">
+        <div className="testimonial" id="testimonials">
             <div className="testimonial-container">
                 <div className='title'>
                     <ScreenHeading title={"Testimonials"} subHeading={"What colleagues say about me."} />
                 </div>
-                <div className="row">
-                    <OwlCarousel className="owl-carousel" id="testimonial-carousel">
-                        <div className="carousel-container">
-                            <div className="testimonial-item">
-                                <div className="testimonial-comment">
-                                    <p>
-                                        <i className="fa fa-quote-left" />
-                                        Christie is the best!
-                                        <i className="fa fa-quote-right" />
-                                    </p>
+                <div className="bg-photo">
+                    <div className="row">
+                        <OwlCarousel className="owl-carousel" id="testimonial-carousel"
+                            {...carousel_options}>
+                            <div className="col-lg-12">
+                                <div className="testimonial-item">
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            <FormatQuoteRoundedIcon className="quote-icon" />
+                                            Christie is the best!
+                                        </p>
+                                    </div>
+                                    <div className="client-info">
+                                        <div className="picture-container">
+                                            <img src="https://dummyimage.com/100x100/000/fff" alt="placeholder" />
+                                        </div>
+                                        <h5>Jane Doe</h5>
+                                        <p>Work Position</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </OwlCarousel>
+                            <div className="col-lg-12">
+                                <div className="testimonial-item">
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            <FormatQuoteRoundedIcon className="quote-icon" />
+                                            Christie is the best!
+                                        </p>
+                                    </div>
+                                    <div className="client-info">
+                                        <div className="picture-container">
+                                            <img src="https://dummyimage.com/100x100/000/fff" alt="placeholder" />
+                                        </div>
+                                        <h5>Jane Doe</h5>
+                                        <p>Work Position</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-12">
+                                <div className="testimonial-item">
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            <FormatQuoteRoundedIcon className="quote-icon" />
+                                            Christie is the best!
+                                        </p>
+                                    </div>
+                                    <div className="client-info">
+                                        <div className="picture-container">
+                                            <img src="https://dummyimage.com/100x100/000/fff" alt="placeholder" />
+                                        </div>
+                                        <h5>Jane Doe</h5>
+                                        <p>Work Position</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-12">
+                                <div className="testimonial-item">
+                                    <div className="testimonial-comment">
+                                        <p>
+                                            <FormatQuoteRoundedIcon className="quote-icon" />
+                                            Christie is the best!
+                                        </p>
+                                    </div>
+                                    <div className="client-info">
+                                        <div className="picture-container">
+                                            <img src="https://dummyimage.com/100x100/000/fff" alt="placeholder" />
+                                        </div>
+                                        <h5>Jane Doe</h5>
+                                        <p>Work Position</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </OwlCarousel>
+                    </div>
                 </div>
             </div>
         </div>
