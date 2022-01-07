3bb3305899b1a2e489fa01a76dddff4fa584a78f
Christie Tsang <ctsang195@gmail.com>
2022-01-07 03:19:49 -0800

    changes to exclusion

--- server.js
+++ server.js
@@ -11,5 +11,12 @@ app.use(express.json());
 app.use(cors());
 app.use("/", contactRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("route/build"))
+    app.get("*", (req, res) => (
+        res.sendFile(path.resolve(__dirname,"route", "build", "index.html"))
+    ))
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listening to port 5000 only`));
